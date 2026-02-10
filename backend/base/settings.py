c2d28d1b36b72b3d9326c4adecb060e57e9d1ddf
Ayax111-debug <ayax2021xd@gmail.com>
2026-02-10 15:33:39 -0300

    Implementación de manejo de errores
         - Se creo la carpeta utils
         - Se creo un archivo exceptions.py
    
         Se implemento un nuevo exception handler que permite que los
         errores que surjen de las reglas de negocios implementadas en los
         modelos sean procesados como un error 400 en vez de un 500

--- backend/base/settings.py
+++ backend/base/settings.py
@@ -134,7 +134,10 @@ REST_FRAMEWORK = {
         ),
         'DEFAULT_PAGINATION_CLASS': 
             'rest_framework.pagination.PageNumberPagination',
-            'PAGE_SIZE':10  
+            'PAGE_SIZE':10, 
+
+        'EXCEPTION_HANDLER':'modulo_principal.utils.exceptions.custom_exception_handler',
+        
     }
 
 SIMPLE_JWT = {
